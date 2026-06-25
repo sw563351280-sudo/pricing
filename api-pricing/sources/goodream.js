4aebbbaf54605d8e24314f7a5155446e34962171
sw563351280-sudo <sw563351280@gmail.com>
2026-06-26 02:05:18 +0900

    Rename: 好梦, 幸运午餐, 转转

--- api-pricing/sources/goodream.js
+++ api-pricing/sources/goodream.js
@@ -1,2 +1,2 @@
 const { createFetcher } = require("./_oneapi-base");
-module.exports = { fetch: createFetcher({ api_url: "https://goodreamapi.online", source: "goodreamapi.online", display: "Goodream" }) };
+module.exports = { fetch: createFetcher({ api_url: "https://goodreamapi.online", source: "goodreamapi.online", display: "好梦" }) };
--- api-pricing/sources/luckylunch.js
+++ api-pricing/sources/luckylunch.js
@@ -1,2 +1,2 @@
 const { createFetcher } = require("./_oneapi-base");
-module.exports = { fetch: createFetcher({ api_url: "https://luckylunch.cc", source: "luckylunch.cc", display: "LuckyLunch" }) };
+module.exports = { fetch: createFetcher({ api_url: "https://luckylunch.cc", source: "luckylunch.cc", display: "幸运午餐" }) };
